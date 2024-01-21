cbcae5c4d7a94c28ba32bb4c6cea5e32616654b9
aagaona <aagaona13@gmailcom>
2024-01-20 19:32:40 -0800

    adding lab files for working React lab. CSS is styled and conditions are met on the App.js

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
